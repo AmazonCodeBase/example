abec410532054e63f57709fd139b9418a694aee1
Pratik Joshi <prajosh25@gmail.com>
2019-05-31 18:05:06 +0530

    Intial commit

---
+++ example/src/main/java/example/Solution.java
@@ -0,0 +1,29 @@
+package example;
+
+import java.util.*;
+class Solution {
+    int solution(int[] A) {
+        int n = A.length;
+        int[] L = new int[n + 1];
+        L[0] = -1;
+        for (int i = 0; i < n; i++) {
+            L[i + 1] = A[i];
+        }
+        int count = 0;
+        int pos = (n + 1) / 2;
+        int candidate = L[pos];
+        for (int i = 1; i < n; i++) {
+            if (L[i] == candidate)
+                count = count + 1;
+        }
+        if (count > pos)
+            return candidate;
+        return (-1);
+    }
+    
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    	int[] A = {1, 1, 1, 1, 50};
+    	System.out.println(new Solution().solution(A));
+	}
+}
